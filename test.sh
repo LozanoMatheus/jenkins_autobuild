41fb1ba4ed2128f362f39439f6541e9b6427a0de
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-05 15:51:50 +0100

    Executing tests on autobuild process.

---
+++ test.sh
@@ -0,0 +1,9 @@
+PING_URL="/metrics/L_O1sN7KtKIC2C1ZV9BiJX7RDTc_f8Iky4pG_Al8PlT7A3EC1zr7oEfU53S6jTPa/ping"
+sleep 5
+if curl jenkins$PING_URL | grep -q 'pong'; then
+  echo "Tests passed!"
+  exit 0
+else
+  echo "Tests failed!"
+  exit 1
+fi
