42542f683177a74d0a0ec4721d83276402a9bb1d
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-05 17:57:10 +0100

    Add shebang + fix curl

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 PING_URL="/metrics/SdQQ16tu23EA94AtNqp_zmMyq0GyO2V85N1TkH4a2BTqeWIFXRZaUq9CqrCrPSqk/ping"
 sleep 60s
-docker ps
-if curl -s http://localhost:80$PING_URL | grep -q 'pong'; then
+if curl -v jenkins | grep -q 'pong'; then
   echo "Tests passed!"
   exit 0
 else
