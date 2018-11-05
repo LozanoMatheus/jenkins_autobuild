cb9500c782553049a4789b8710ab9d7f78303eac
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-05 16:10:43 +0100

    Change sleep time

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 PING_URL="/metrics/L_O1sN7KtKIC2C1ZV9BiJX7RDTc_f8Iky4pG_Al8PlT7A3EC1zr7oEfU53S6jTPa/ping"
-sleep 5
+sleep 5s
 if curl jenkins$PING_URL | grep -q 'pong'; then
   echo "Tests passed!"
   exit 0
