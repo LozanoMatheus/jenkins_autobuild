c1a26e0c7fa4361d38558f15e58aa7309414f47f
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-05 17:13:16 +0100

    Fix request to healthcheck

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 PING_URL="/metrics/SdQQ16tu23EA94AtNqp_zmMyq0GyO2V85N1TkH4a2BTqeWIFXRZaUq9CqrCrPSqk/ping"
 sleep 60s
-if curl jenkins$PING_URL | grep -q 'pong'; then
+if curl -s http://localhost:80$PING_URL | grep -q 'pong'; then
   echo "Tests passed!"
   exit 0
 else
