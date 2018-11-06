d921f54f47903ec21c462febd295960c82f86f9f
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-06 17:28:31 +0100

    Test files to run with autotest/autobuild via docker cloud

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PING_URL="/metrics/SdQQ16tu23EA94AtNqp_zmMyq0GyO2V85N1TkH4a2BTqeWIFXRZaUq9CqrCrPSqk/ping"
+sleep 60s
+if curl -s jenkins:8080$PING_URL | grep -i "pong" ; then
+  echo "Tests passed!"
+  exit 0
+else
+  echo "Tests failed!"
+  exit 1
+fi
