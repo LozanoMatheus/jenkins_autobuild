8396ece1c098f8fcda5ad491b81777aa4c3d57fd
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-06 17:48:16 +0100

    Trying with IP address

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 PING_URL="/metrics/SdQQ16tu23EA94AtNqp_zmMyq0GyO2V85N1TkH4a2BTqeWIFXRZaUq9CqrCrPSqk/ping"
 sleep 60s
-if curl -s jenkins:8080$PING_URL | grep -i "pong" ; then
+if curl -v http://172.17.0.2:8080$PING_URL | grep -i "pong" ; then
   echo "Tests passed!"
   exit 0
 else
