bdadfea338308d129ca66ef3065e243e2c266ce6
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-07 12:03:58 +0100

    Executing fully tests

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 PING_URL="/metrics/SdQQ16tu23EA94AtNqp_zmMyq0GyO2V85N1TkH4a2BTqeWIFXRZaUq9CqrCrPSqk/ping"
 ss -tuan
-docker ps
-docker-compose ps
+docker ps || true
+docker-compose ps || true
 sleep 60s
-if curl -v http://localhost:8080$PING_URL | grep -i "pong" ; then
+if curl -v http://jenkins:8080$PING_URL | grep -i "pong" ; then
   echo "Tests passed!"
   exit 0
 else
