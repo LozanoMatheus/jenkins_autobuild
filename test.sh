9ffdee4241fb060c4adbe24f124c5bc84398f46d
Matheus Lozano <lozanomatheus@gmail.com>
2018-11-07 11:42:27 +0100

    Trying to find container IP.

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PING_URL="/metrics/SdQQ16tu23EA94AtNqp_zmMyq0GyO2V85N1TkH4a2BTqeWIFXRZaUq9CqrCrPSqk/ping"
+ss -tuan
+docker ps
+docker-compose ps
 sleep 60s
 if curl -v http://172.17.0.2:8080$PING_URL | grep -i "pong" ; then
   echo "Tests passed!"
